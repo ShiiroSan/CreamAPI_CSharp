0d2cd9647920c56fb0d72a4092ae5d6487aa6c8e
Anaë Yuurei <eaudrey96@gmail.com>
2018-06-13 09:14:42 +0200

    Ajoutez des fichiers projet.

---
+++ CreamAPI_CSharp/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using IniParser;
+using IniParser.Model;
+using Microsoft.Win32;
+
+namespace CreamAPI_CSharp
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string caFileConfig = "caconfig.ini";
+            bool bConfigShow;
+            if (File.Exists(caFileConfig))
+            {
+                var iniParser = new FileIniDataParser();
+                IniData data = iniParser.ReadFile(caFileConfig);
+                string sLangCfg = data["default"]["language"];
+                if (sLangCfg == "")
+                {
+                    sLangCfg = "English";
+                }
+                string sExtraProtecBypass = data["default"]["extraProtectionBypass"];
+                if (sExtraProtecBypass == "")
+                {
+                    sExtraProtecBypass = "False";
+                }
+                bool bExtraProtecBypass = bool.Parse(sExtraProtecBypass);
+                string sOfflineMode = data["default"]["offlineMode"];
+                if (sOfflineMode == "")
+                {
+                    sOfflineMode = "False";
+                }
+                bool bOfflineMode = bool.Parse(sOfflineMode);
+                string sUserdataFolder = data["default"]["userdataFolder"];
+                if (sUserdataFolder == "")
+                {
+                    sUserdataFolder = "False";
+                }
+                bool bUserdataFolder = bool.Parse(sUserdataFolder);
+                string sLowViolence = data["default"]["lowviolence"];
+                if (sLowViolence == "")
+                {
+                    sLowViolence = "False";
+                }
+                bool bLowViolence = bool.Parse(sLowViolence);
+                string sWrapperMode = data["default"]["wrapperMode"];
+                if (sWrapperMode == "")
+                {
+                    sWrapperMode = "False";
+                }
+                bool bWrapperMode = bool.Parse(sWrapperMode);
+                Console.WriteLine(sLangCfg);
+                bConfigShow = false;
+            }
+            else
+        	{
+                //Show Config gui
+                bConfigShow = true;
+            }
+
+            #region Get Steam Path
+            var sBaseDirSteam = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Valve\\Steam\\", "SteamPath", "C:\\Program Files(x86)\\Steam");
+            //var sConfSteamFile = File.Open(sBaseDirSteam + "\\config\\config.vdf",FileMode.Open);
+            //var sConfFileRead = sConfSteamFile.Read();
+            //getSteamPath with Regex
+            string gameDir = "";
+            #endregion Get Steam Path
+            bool noGame;
+            if (gameDir == "")
+            {
+                noGame = true;
+            }
+            else
+            {
+                noGame = false;
+            }
+            if (!noGame)
+            {
+                //Split game path to find name
+
+            }
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new searchGameForm());
+
+        }
+
+
+    }
+}
---
+++ CreamAPI_CSharp/searchGameForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreamAPI_CSharp
+{
+    public partial class searchGameForm : Form
+    {
+        public searchGameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void searchGameBtn_Click(object sender, EventArgs e)
+        {
+            if (searchGameInputBox.Text == "" || searchGameInputBox.Text == "Write game name to search here...")
+            {
+                MessageBox.Show("You must specify a game name!", "Error!");
+                return;
+            }
+
+        }
+    }
+}
